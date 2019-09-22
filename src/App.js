f6728fbfbd1a8001c5dcc83646d982f6a0c4ed01
Vuochly Ky <ninvk@uw.edu>
2019-09-22 16:03:32 -0700

    updating our app with a shop page, CollectionPreview component and CollectionItemcomponent

--- src/App.js
+++ src/App.js
@@ -3,21 +3,14 @@ import {Switch, Route} from 'react-router-dom';
 //import './App.css';
 
 import HomePage from './Pages/homepage';
-
-//npm install -g sass
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from './Pages/shop/shop.jsx';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} /> {/*exact is basically exact={true} */}
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
