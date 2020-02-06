9e8e68db88f8f7882607f42f43a9dd66e4e945f8
Vuochly Ky <ninvk@uw.edu>
2020-02-06 14:41:52 -0800

    127 Redux Persist (sessionStorage)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { PersistGate} from "redux-persist/integration/react";
+
+import { store, persistor } from './redux/store';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import store from './redux/store';
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App />
+            <PersistGate persistor={persistor}> {/* Wrap the App to make the app always have access to the persistence flow itself  */}
+                <App />
+            </PersistGate>
         </BrowserRouter>
     </Provider>,
     document.getElementById('root')
